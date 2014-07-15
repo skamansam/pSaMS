5f9d1793e74f88aaf389b1a1c87811085462e25a
Samuel C. Tyler <skamansam@gmail.com>
2014-07-15 09:26:08 -0400

    better tag cloud handling

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -8,12 +8,12 @@ PSaMs::App.helpers do
     link_to account.name+' '+account.surname, 'mailto:'+account.email
   end
 
-  def post_tag_cloud(tags = Post.tag_counts_on(:tags))
-    out = ""
+  def post_tag_cloud(tags = Post.tag_counts_on(:tags), separator = " | ")
+    out = []
     tag_cloud(tags, %w(cloud1 cloud2 cloud3 cloud4)) do |tag, css_class|
-      out+="\n"+link_to( tag.name, url_for(:posts, :tags, :tag_name=>tag.name), :class => css_class)
+      out << link_to( tag.name, url_for(:posts, :tags, :tag_name=>tag.name), :class => css_class)
     end
-    out.html_safe
+    out.join(separator).html_safe
   end
 
 end
