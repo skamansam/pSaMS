73a2c92ed64cc87e5a18a5a794d2236c4d6be49b
Samuel C. Tyler <skamansam@gmail.com>
2014-11-15 23:31:06 -0500

    cleaned up plain text editor [Finishes #1]

--- plugins/core_editor/plain_editor.rb
+++ plugins/core_editor/plain_editor.rb
@@ -1,34 +1,21 @@
 class PlainEditor
-  include Haml
 
   def self.info
-    ["Core Editor: Plain v1.0","Adds a plain-text editor, a markdown editor, and an HTML editor for posts."]
+    ["Core Editor: Plain v1.0","Adds a plain-text editor for posts."]
   end
 
   def initialize
-    logger.info "Initializing CoreEditor"
+    logger.info "Initializing CoreEditor/PlainEditor"
   end
-
-  def self.migrate ; end
 
   def self.setup
     Plugin.add_action('add_editor', 'admin.posts.edit', "PlainEditor", 'get_post_editor',0)
-    Plugin.add_action('add_css', 'admin.posts.edit', "PlainEditor", 'get_css',0)
-    Plugin.add_action('add_js', 'admin.posts.edit', "PlainEditor", 'get_js',0)
   end
 
   def get_post_editor(context,form,error)
     { name: 'Plain Text',
       body: context.partial("core_editor/html/plain_editor.haml", locals: {f: form, error: error})
     }
-  end
-
-  def get_css
-    "<!-- SimpleEditor CSS goes here -->".html_safe
-  end
-
-  def get_js
-    "<!-- SimpleEditor JS goes here -->".html_safe
   end
 
 end
