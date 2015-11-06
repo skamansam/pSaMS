9d88e3ff4e1a78a309d598425f107fb69b5dc1bd
Samuel C. Tyler <skamansam@gmail.com>
2015-11-06 10:45:25 -0500

    add back manual ordering of categories

--- models/category.rb
+++ models/category.rb
@@ -18,7 +18,7 @@ class Category < ActiveRecord::Base
   has_many :children, :class_name=>Category, :foreign_key=>:parent_id
   has_many :posts
 
-  default_scope { order('item_order DESC') }
+  default_scope { order('item_order ASC') }
 
   def all_posts
     return self.posts + self.children.map(&:all_posts)
